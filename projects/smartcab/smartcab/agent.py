2f8700f3465ff5e117795fec14785b52ffb60422
nirvanafish <fish820124@163.com>
2016-08-19 15:58:27 +0800

    update p4 code for furthur improvement

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -2,6 +2,8 @@ import random
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
+from collections import OrderedDict
+import numpy as np
 
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
@@ -11,28 +13,63 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.epsilon = 0.5
+        self.alpha = 0.3
+        self.discount = 0.9
+        self.Qvalue = OrderedDict()
+        self.state = None
+
+
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+        # self.Qvalue = OrderedDict()        
 
     def update(self, t):
         # Gather inputs
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
-
+        #self.epsilon = self.epsilon * deadline/100
         # TODO: Update state
-        
+        self.state = (inputs['light'], inputs['oncoming'], inputs['left'], inputs['right'], self.next_waypoint)
+
+        for action in self.env.valid_actions:
+            if (self.state, action) not in self.Qvalue.iterkeys():
+                self.Qvalue[(self.state, action)] = 0
+           
+        prob = [self.Qvalue[(self.state, None)] , self.Qvalue[(self.state, 'forward')], self.Qvalue[(self.state, 'left')], self.Qvalue[(self.state, 'right')]]
+        prob = np.exp(prob)/np.sum(np.exp(prob), axis = 0)
+
         # TODO: Select action according to your policy
-        action = None
+
+        if random.random() < self.epsilon:
+            action = np.random.choice(self.env.valid_actions, p=prob)
+        else:
+            action = self.env.valid_actions[np.argmax(prob)]
+ 
 
         # Execute action and get reward
+        oldValue = self.Qvalue[(self.state, action)]
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        #nextInputs = self.env.sense(self)
+        #print inputs, nextInputs
+        #nextState = (nextInputs['light'], nextInputs['oncoming'], nextInputs['left'], nextInputs['right'], self.planner.next_waypoint())
+
+        #for action in self.env.valid_actions:
+        #    if (nextState, action) not in self.Qvalue.iterkeys():
+        #        self.Qvalue[(nextState, action)] = 0
+
+        #futureValue = max([self.Qvalue[(nextState, a)] for a in self.env.valid_actions])
+        newValue = reward #+ self.discount * futureValue 
+        self.Qvalue[(self.state, action)] =  (1-self.alpha) * oldValue + self.alpha * newValue
+        
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]A
+        #print self.state, action, self.Qvalue[(self.state, action)], nextState, futureValue
 
 
 def run():
@@ -45,12 +82,15 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.01, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
+    for (state, action) in a.Qvalue.iterkeys(): 
+        print (state, action), a.Qvalue[(state, action)]
 
+    Simulator(e, update_delay = 1, display = True).run(n_trials = 5)
 
 if __name__ == '__main__':
     run()
